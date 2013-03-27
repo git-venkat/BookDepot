78e0d0f1679208cc6f4ddecf05ff8537562a80b4
Venkat <venkat@venkat-Inspiron-N5050.(none)>
2013-03-27 11:05:24 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#BookDepot Application
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '644906765fa7750c0f0a7ffef1cfeee2d18718c70cde69e2a53419c75e97b55995dcf971cca7b0f24cd5277e09473db80d4eb13c792b4c3dcdc7f7c8d931640c'
